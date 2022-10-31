aeabd28c9d042b89aba3314fd44daba1519f7025
liuzehao <1437070389@qq.com>
2022-10-31 18:01:16 +0800

    大一上学期写的终端万年历

--- my_cal/flq.cpp
+++ my_cal/flq.cpp
@@ -6,8 +6,9 @@
 
 using namespace std;
 bool IS_Leap = false;
-int main(int argc, char **argv) {
-  system("cls");
+int main(int argc, char **argv)
+{
+  system(CLEAR);
   int iMonths[LEN];
   struct tm *tp;
   time_t tTime;
@@ -20,12 +21,13 @@ int main(int argc, char **argv) {
   const char name[] = "flq";
   const char name_other[] = "flq.exe";
 #endif
-  switch (argc) {
+  switch (argc)
+  {
   case FIR:
     if (!strcmp(name, argv[0])) // 显示本年本月日历
     {
       Fdm(iFdy(tp->tm_year + BASE), tp->tm_year + BASE,
-          iMonths); // 获得一个每个元素为指定年份每一个月一号的星期数的数组
+          iMonths);                          // 获得一个每个元素为指定年份每一个月一号的星期数的数组
       is_leap(tp->tm_year + BASE, &IS_Leap); // 判断该年是否为闰年
       int range1[2] = {tp->tm_mon + 1, tp->tm_mon + 1};
       function_L2(iMonths, range1, IS_Leap); // 显示指定年份指定月份日历
@@ -42,30 +44,37 @@ int main(int argc, char **argv) {
       Fdm(iFdy(tp->tm_year + BASE), tp->tm_year + BASE, iMonths);
       is_leap(tp->tm_year + BASE, &IS_Leap);
       function_L2(iMonths, iMonthRange, IS_Leap);
-    } else if (strlen(argv[1]) == 1) // 显示本年指定某个月份日历
+    }
+    else if (strlen(argv[1]) == 1) // 显示本年指定某个月份日历
     {
       char *end;
       long lMonth = strtol(argv[1], &end, NUMBASE);
-      if (end == argv[1]) {
+      if (end == argv[1])
+      {
         cout << "参数错误" << endl;
         cout << "请使用-h参数查看使用方法" << endl;
         exit(0);
-      } else {
+      }
+      else
+      {
         Fdm(iFdy(tp->tm_year + BASE), tp->tm_year + BASE, iMonths);
         is_leap(tp->tm_year + BASE, &IS_Leap);
         int iRANGE[2] = {lMonth, lMonth};
         function_L2(iMonths, iRANGE, IS_Leap);
       }
-    } else if (strlen(argv[1]) == 2 || strlen(argv[1]) == 3 ||
-               strlen(argv[1]) == 4 ||
-               strlen(argv[1]) == 5) // 指定某范围内本年的日历
+    }
+    else if (strlen(argv[1]) == 2 || strlen(argv[1]) == 3 ||
+             strlen(argv[1]) == 4 ||
+             strlen(argv[1]) == 5) // 指定某范围内本年的日历
     {
       int iMonthRange[RANGE];
       param_two(argv[1], iMonthRange);
       Fdm(iFdy(tp->tm_year + BASE), tp->tm_year + BASE, iMonths);
       is_leap(tp->tm_year + BASE, &IS_Leap);
       function_L2(iMonths, iMonthRange, IS_Leap);
-    } else {
+    }
+    else
+    {
       cout << "参数范围错误" << endl;
       exit(0);
     }
@@ -73,35 +82,47 @@ int main(int argc, char **argv) {
   case THR: // 指定年份指定月份输出日历
     char *end;
     long lYear = strtol(argv[1], &end, NUMBASE);
-    if (end == argv[1]) {
+    if (end == argv[1])
+    {
       cout << "参数错误" << endl;
       cout << "请使用-h参数查看使用方法" << endl;
       exit(0);
     }
-    if (lYear <= 0) {
+    if (lYear <= 0)
+    {
       cout << "年份错误" << endl;
       exit(0);
-    } else {
-      if (!strcmp(argv[2], WHOLEYEAR)) {
+    }
+    else
+    {
+      if (!strcmp(argv[2], WHOLEYEAR))
+      {
         int iMonthRange[RANGE];
         iMonthRange[0] = 1;
         iMonthRange[1] = MONTHS;
         Fdm(iFdy(lYear), lYear, iMonths);
         is_leap(lYear, &IS_Leap);
         function_L2(iMonths, iMonthRange, IS_Leap);
-      } else if (strlen(argv[2]) == 1) {
+      }
+      else if (strlen(argv[2]) == 1)
+      {
         int a = argv[2][0] - '0';
-        if (a >= 1 && a <= MONTHS) {
+        if (a >= 1 && a <= MONTHS)
+        {
           Fdm(iFdy(lYear), lYear, iMonths);
           is_leap(lYear, &IS_Leap);
           int iRange[2] = {a, a};
           function_L2(iMonths, iRange, IS_Leap);
-        } else {
+        }
+        else
+        {
           cout << "参数错误" << endl;
           exit(0);
         }
-      } else if (strlen(argv[2]) == 2 || strlen(argv[2]) == 3 ||
-                 strlen(argv[2]) == 4 || strlen(argv[2]) == 5) {
+      }
+      else if (strlen(argv[2]) == 2 || strlen(argv[2]) == 3 ||
+               strlen(argv[2]) == 4 || strlen(argv[2]) == 5)
+      {
         int iMonthRange[RANGE];
         param_two(argv[2], iMonthRange);
         Fdm(iFdy(lYear), lYear, iMonths);
